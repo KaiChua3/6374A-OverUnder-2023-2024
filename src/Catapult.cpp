124fef800f4927202bbb78d4215b89fc900d96e0
KaiChua3 <555725@my.cuhsd.org>
2024-01-31 20:18:42 -0800

    Added extra catapult motor

--- src/Catapult.cpp
+++ src/Catapult.cpp
@@ -5,7 +5,7 @@
 #include "Catapult.hpp"
 #include "pros/rtos.hpp"
 
-Catapult::Catapult (int8_t const cata_port) : 
+Catapult::Catapult (int8_t const cata_port, int8_t const small_cata_port) : 
 cata_motor{cata_port}, 
 small_cata_motor{small_cata_port} {}
 
